21456379bfdde33e893243e9b0edc69f63ce359d
Quentin Joubert <quentin.joubert28@gmail.com>
2017-11-20 12:09:45 +0100

    ajout du JSON

---
+++ RecupHistoire.php
@@ -0,0 +1,60 @@
+﻿<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Mad Maxi Jack</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+
+<?php
+$dsn = 'mysql:dbname=histoire_interactive;host=localhost';
+$user = 'root';
+$password = '';
+
+try {
+	$dbh = new PDO($dsn, $user, $password);
+}
+
+catch (PDOException $e) {
+    echo 'Connexion échouée : ';
+}
+$stmt = $dbh->query('SELECT * FROM page');
+
+?>
+<body id = "MadMaxiJack" onload='scene(0)'>
+
+<div id="Mad_Maxi-Jack">
+	
+	<?php
+	$i = 0;
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+ ?>
+ <div class="Scene" id=<?php echo $i?>> <!--page 1-->
+ <img src="<?php echo $row['Image']?>"/>
+
+ <p><?php echo $row['Texte']?></p>
+
+ <?php
+$stmt2 = $dbh->query("SELECT * FROM choix where IdPageProposition = $i ");
+
+	?> 
+	<input class = 'bouton' type='button' value='suivant' OnClick='scene(<?php echo $i + 1 ?>)'>
+<?php
+
+while($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+ ?>
+ 
+<input class = 'bouton' type='button' value=<?php echo $row2['TextProposition']?> OnClick= 'scene(<?php echo $row2['IdPagePropose'] ?>)'>
+<?php
+}
+?> 
+<?php
+$i = $i+1;
+echo "</div>";
+}
+?>
+</div>
+</body>
+</html>
--- script.js
+++ script.js
@@ -13,3 +13,102 @@ function CacherContact()
 	lienContact.text = "Contactez-Nous";
 	lienContact.onclick = AfficherContact;
 }
+
+function scene(SceneX){
+	console.log(SceneX);
+	Cacher();
+	Afficher(SceneX);
+}
+
+
+function Cacher()
+{
+	var listScene = document.getElementsByClassName("Scene");
+	for (var i=0; i<listScene.length; i++){
+		listScene[i].style.display = "none";
+	}
+	
+}
+
+function Afficher(SceneX)
+{
+	document.getElementById(String(SceneX)).style.display = "block";
+}
+
+var monhist={
+	"title": "Mad_Maxi-Jack",
+	"scenes": [{
+			"id": 0,
+			"urlimag": "img/0.png",
+			"text": "Maxi-Jack coule des jours heureux une fois de plus, mais une soudaine intuition le pousse à penser qu'il ne profitera pas de ces moments bénis trop longtemps.",
+			"choices": [{
+				"text": "suivant",
+				"output": 1
+			}],
+			"Fin": 0
+		},
+
+		{
+			"id": 1,
+			"urlimag": "img/1.png",
+			"text": "Car, suite à une négligence humaine, la planète subit des dommages irréversibles au niveau écologique et en terme d'organisation de la société également.",
+			"choices": [{
+				"text": "suivant",
+				"output": 2
+			}],
+			"Fin": 0
+		},
+		{
+			"id": 2,
+			"urlimag": "img/2.png",
+			"text": "Car, suite à une négligence humaine, la planète subit des dommages irréversibles au niveau écologique et en terme d'organisation de la société également.",
+			"choices": [{
+				"text": "suivant",
+				"output": 3
+			}],
+			"Fin": 0
+		}
+	]
+};
+
+function creerHistoire (objet)
+{
+	histoire = document.createElement("div");
+	histoire.id = objet.title;
+	
+	var i = 0
+	for (var i = 0; i in objet.scenes; i++) {
+		vignette = document.createElement("div");
+		vignette.className = "Scene";
+		vignette.id = i;
+		
+		monimg = document.createElement("img");
+		monimg.setAttribute("src", objet.scenes[i].urlimag);
+		
+		montxt = document.createElement("p");
+		t = document.createTextNode(objet.scenes[i].text);
+		montxt.appendChild(t);
+		
+		vignette.appendChild(monimg);
+		vignette.appendChild(montxt);
+		
+		for (var j = 0; j in objet.scenes[i].choices; j++) {
+			choix = document.createElement("BUTTON");
+			choix.className = "bouton";
+			t = document.createTextNode(objet.scenes[i].choices[j].text);
+			choix.setAttribute("name", objet.scenes[i].choices[j].output);
+			choix.appendChild(t)
+			choix.onclick = function(){scene(this.getAttribute("name"))};
+			
+			vignette.appendChild(choix);
+		}
+		histoire.appendChild(vignette);
+		document.body.appendChild(histoire);
+	}
+}
+
+function initialiser()
+{
+	creerHistoire(monhist);
+	scene(0);
+}

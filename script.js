7bb88963ae9e200846b960787342cfbfdaaea00f
Quentin Joubert <quentin.joubert28@gmail.com>
2017-11-29 10:06:48 +0100

    fichier JSON

--- script.js
+++ script.js
@@ -35,82 +35,56 @@ function Afficher(SceneX)
 	document.getElementById(String(SceneX)).style.display = "block";
 }
 
-var monhist={
-	"title": "Mad_Maxi-Jack",
-	"scenes": [{
-			"id": 0,
-			"urlimag": "img/0.png",
-			"text": "Maxi-Jack coule des jours heureux une fois de plus, mais une soudaine intuition le pousse à penser qu'il ne profitera pas de ces moments bénis trop longtemps.",
-			"choices": [{
-				"text": "suivant",
-				"output": 1
-			}],
-			"Fin": 0
-		},
-
-		{
-			"id": 1,
-			"urlimag": "img/1.png",
-			"text": "Car, suite à une négligence humaine, la planète subit des dommages irréversibles au niveau écologique et en terme d'organisation de la société également.",
-			"choices": [{
-				"text": "suivant",
-				"output": 2
-			}],
-			"Fin": 0
-		},
-		{
-			"id": 2,
-			"urlimag": "img/2.png",
-			"text": "Car, suite à une négligence humaine, la planète subit des dommages irréversibles au niveau écologique et en terme d'organisation de la société également.",
-			"choices": [{
-				"text": "suivant",
-				"output": 3
-			}],
-			"Fin": 0
-		}
-	]
-};
-
-function creerHistoire (objet)
+function initialiserHistoire (LienJSON)
 {
-	histoire = document.createElement("div");
-	histoire.id = objet.title;
-	
-	var i = 0
-	for (var i = 0; i in objet.scenes; i++) {
-		vignette = document.createElement("div");
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var JSONhistoire = JSON.parse(this.responseText);
+
+	var histoire = document.createElement("div");
+	histoire.id = JSONhistoire.title;
+
+	for (var i = 0; i in JSONhistoire.scenes; i++) {
+		var vignette = document.createElement("div");
 		vignette.className = "Scene";
 		vignette.id = i;
 		
-		monimg = document.createElement("img");
-		monimg.setAttribute("src", objet.scenes[i].urlimag);
+		var monimg = document.createElement("img");
+		monimg.setAttribute("src", JSONhistoire.scenes[i].urlimag);
 		
-		montxt = document.createElement("p");
-		t = document.createTextNode(objet.scenes[i].text);
-		montxt.appendChild(t);
+		var montxt = document.createElement("p");
+		var monspan = document.createElement("span");
+		monspan.className = "montext";
+		var t = document.createTextNode(JSONhistoire.scenes[i].text);
+		monspan.appendChild(t);
+		montxt.appendChild(monspan);
 		
 		vignette.appendChild(monimg);
 		vignette.appendChild(montxt);
 		
-		for (var j = 0; j in objet.scenes[i].choices; j++) {
-			choix = document.createElement("BUTTON");
+		for (var j = 0; j in JSONhistoire.scenes[i].choices; j++) {
+			var choix = document.createElement("BUTTON");
 			choix.className = "bouton";
-			t = document.createTextNode(objet.scenes[i].choices[j].text);
-			choix.setAttribute("name", objet.scenes[i].choices[j].output);
-			choix.appendChild(t)
+			var txt = document.createTextNode(JSONhistoire.scenes[i].choices[j].text);
+			choix.setAttribute("name", JSONhistoire.scenes[i].choices[j].output);
+			choix.appendChild(txt)
 			choix.onclick = function(){scene(this.getAttribute("name"))};
 			
 			vignette.appendChild(choix);
 		}
 		histoire.appendChild(vignette);
 		document.body.appendChild(histoire);
+		
+		if(xmlhttp.readyState == 4) 
+      { 
+		scene(0);
+	  }
 	}
-}
-
-function initialiser()
-{
-	creerHistoire(monhist);
-	scene(0);
+	}
+	};
+xmlhttp.open("GET", LienJSON, true);
+xmlhttp.send();
 }
 
 function Choix(num){
